d2fd35907145f183e697f4d1d555d51f65c4d6af
Matthew Cook <mcookhome@gmail.com>
2018-05-29 00:33:29 -0400

    Changed program to pick most seen track of four different queries

--- billboard_to_spotify.py
+++ billboard_to_spotify.py
@@ -1,6 +1,8 @@
 import billboard
+import argparse
 from util import spotify_api_helper
 import re
+import collections
 
 DEFAULT_USERNAME = "mcookhome"
 
@@ -16,35 +18,55 @@ def run(chart_name, date):
 def convert_chart_to_uri_list(sp, chart):
     track_uris = []
     count_not_found = 0
+    chart_num = 1
+    year = chart.date[0:4]
+    print(year)
     for track in chart:
+        print(str(chart_num) + ": ")
+        print(track)
         name_query = re.sub(r" ?\([^)]+\)", "", track.title)
-        name_and_artist_query = name_query + " - " + track.artist
-        name_and_partial_artist_query = name_query + " " + " ".join(track.artist.split(" ")[0:2])
-        #print(name_and_artist_query)
-        queries = [name_query, name_and_artist_query, name_and_partial_artist_query]
-        all_results = [{"query": query, "result" : spotify_api_helper.search_billboard(sp, query)} for query in queries]
-        for entry in all_results:
-            result = entry["result"]
-            query = entry["query"]
-            print(query)
-            if len(result["tracks"]["items"]) == 1:
-                print(query + ": " + result["tracks"]["items"][0]["name"])
+        artist = re.sub('".*?"', '', track.artist)
+        artist = "".join(artist.split("*"))
+        name_and_artist_query = name_query + " - " + artist
+        name_and_two_word_artist_query = name_query + " " + " ".join(artist.split(" ")[0:2])
+        name_and_one_word_artist_query = name_query + " " + " ".join(artist.split(" ")[0:1])
 
-        #print(all_results)
-        results = spotify_api_helper.search_billboard(sp, name_and_partial_artist_query)
-        if len(results["tracks"]["items"]) == 0:
-            print("Could not find a Spotify track for this query: " + name_and_partial_artist_query)
+        queries = [name_and_two_word_artist_query, name_and_one_word_artist_query, name_query, name_and_artist_query]
+        queries = [query + " NOT instrumental NOT karaoke" for query in queries]
+        full_results_for_queries = {}
+        for query in queries:
+            result = spotify_api_helper.search_billboard(sp, query)
+            if len(result["tracks"]["items"]) != 0:
+                retrieved_track = result["tracks"]["items"][0]
+                value = {"name": retrieved_track["name"], "artists": retrieved_track["artists"][0]["name"]}
+                if retrieved_track["uri"] not in full_results_for_queries:
+                    value["count"] = 1
+                    value["queries"] = [query]
+                    full_results_for_queries[retrieved_track["uri"]] = value
+                else:
+                    #print(full_results_for_queries[retrieved_track["uri"]])
+                    value["count"] = full_results_for_queries[retrieved_track["uri"]]["count"] + 1
+                    value["queries"] = full_results_for_queries[retrieved_track["uri"]]["queries"]
+                    value["queries"].append(query)
+                    full_results_for_queries[retrieved_track["uri"]] = value
+        print(str(full_results_for_queries))
+        if not full_results_for_queries:
             count_not_found = count_not_found + 1
+            print("Could not find a Spotify track for this track: " + name_query)
         else:
-            track = results["tracks"]["items"][0]
-            track_name = track["name"]
-            artist_name = track["artists"][0]["name"]
-            #print(track_name + " - " + artist_name)
-            track_uris.append(track["uri"])
+            chosen_track_uri = max(full_results_for_queries, key=lambda key: full_results_for_queries[key]["count"])
+            track_uris.append(chosen_track_uri)
+        chart_num = chart_num + 1
     print("Sorry, but I couldn't find " + str(count_not_found) + " tracks")
     return track_uris
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--date", type=str, required=True)
+    parser.add_argument("-c", "--chart", type=str, default='hot-100')
+    args = parser.parse_args()
+
     chart_name = 'hot-100'
     date = "2000-01-01"
-    run(chart_name, date)
+    #run(chart_name, date)
+    run(args.chart, args.date)
